b481503082ab5308e4aaf243594bb57ff31c9f18
kilianetter <52922311+kilianetter@users.noreply.github.com>
2021-01-07 21:48:48 +0100

    Update run.py

--- TempHum/run.py
+++ TempHum/run.py
@@ -3,6 +3,11 @@ import Adafruit_DHT as DHT
 import time
 import datetime as dt
 
+from statistics import mean
+
+T = []
+RH = []
+
 
 while True:
 	now = dt.datetime.now()
@@ -10,5 +15,15 @@ while True:
 	if (int(now.strftime("%S"))) == 0: 
 		humidity , temperature = DHT.read_retry(11,4)
 
-		print(f'[{now.strftime("%Y-%m-%d - %H:%M:%S")}] - Temp: {temperature}°C - Humidity: {humidity}%')
+		
+		T.append(temperature)
+		RH.append(humidity)
+		print(len(T)
 		time.sleep(55)
+	
+	if (int(now.strftime("%M"))) == 5:
+		mean_t = mean(T)
+		mean_rh = mean(RH)
+		print(f'[{now.strftime("%Y-%m-%d - %H:%M:%S")}] - Temp: {mean_t}°C - Humidity: {mean_rh}%')
+		T = []
+		RH = []
